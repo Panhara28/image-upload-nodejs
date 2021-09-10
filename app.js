74719ae1bdd2199f4cccb28f027017d5bc926ddf
DPTSI <dptsi@DPTSIs-MacBook-Pro.local>
2021-09-10 20:41:29 +0700

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
